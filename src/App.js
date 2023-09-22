4ec04834b242814fd573685fabc84480c570aec0
mraiyuuu <munenedenis618@gmail.com>
2023-09-23 00:43:25 +0300

    count

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import "./App.css";
 import { Greet } from "./Components/Greet";
 import Welcome from "./Components/Welcome";
 import Hello from "./Components/Hello";
-import Message from './Components/Message'
-import Next from './Components/Next'
+import Message from "./Components/Message";
+import Next from "./Components/Next";
+import Counter from "./Components/Counter";
 function App() {
   return (
     <div className="App">
+      <Counter />
 
-      <Next/>
-
+      {/* <Next/> */}
       {/* <Message /> */}
       {/* <Greet name="Denis" heroName="Trader">
         <p>This is children props</p>
---
+++ src/Components/Counter.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+
+ increment() {
+  this.setState({
+    count: this.state.count + 1
+  })
+ }
+
+  render() {
+    return (
+      <div>
+        <div>Count - {this.state.count}</div>
+        <button onClick={() => this.increment()}>Increment</button>
+      </div>
+    );
+  }
+}
+
+export default Counter
